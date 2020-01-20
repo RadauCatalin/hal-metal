036d56c8c89f1d20f21d042ea891a3be0ccceef9
Catalin <catalin.radau@yahoo.com>
2020-01-20 02:08:44 +0200

    Working on portofolio and emailing

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,6 +1,6 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) ||empty($_POST['place']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
 }
@@ -8,13 +8,14 @@ if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+$dimension = strip_tags(htmlspecialchars($_POST['dimension']));
+$place = strip_tags(htmlspecialchars($_POST['place']));
 
 // Create the email and send the message
-$to = "catalin_zapata@yahoo.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
-$subject = "Website Contact Form:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: noreply@localhost:63342.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$to = "hal.metalsrl@yahoo.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$subject = "Cerere oferta site:  $name";
+$body = "Ai primit o noua cerere de pe site-ul tau.\n\n"."Aici ai detaliile:\n\nNume: $name\n\nEmail: $email\n\nTelefon: $phone\n\nDimensiune constructie:\n$dimension \n\nLocalitate proiect:\n$place";
+$header = "From: office@halmetal.ro\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
 if(!mail($to, $subject, $body, $header))

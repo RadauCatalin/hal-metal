988d5bdc5568e63c3fb7a9a438c0ee6a34c53e67
Catalin <catalin.radau@yahoo.com>
2020-01-20 13:30:49 +0200

    Rearrange code, added img in portofolio, finalized code for email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,8 +1,8 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) ||empty($_POST['place']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
+if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['dimension']) || empty($_POST['place']) || empty($_POST['dimension']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    http_response_code(500);
+    exit();
 }
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
@@ -10,14 +10,15 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $dimension = strip_tags(htmlspecialchars($_POST['dimension']));
 $place = strip_tags(htmlspecialchars($_POST['place']));
+$details = strip_tags(htmlspecialchars($_POST['details']));
 
 // Create the email and send the message
 $to = "hal.metalsrl@yahoo.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Cerere oferta site:  $name";
-$body = "Ai primit o noua cerere de pe site-ul tau.\n\n"."Aici ai detaliile:\n\nNume: $name\n\nEmail: $email\n\nTelefon: $phone\n\nDimensiune constructie:\n$dimension \n\nLocalitate proiect:\n$place";
+$body = "Ai primit o noua cerere de pe site-ul tau.\n\n" . "Aici ai detaliile:\n\nNume: $name\n\nEmail: $email\n\nTelefon: $phone\n\nDimensiune constructie:\n$dimension \n\nLocalitate proiect:\n$place \n\nDetalii constructie:\n$details";
 $header = "From: office@halmetal.ro\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$header .= "Reply-To: $email";	
+$header .= "Reply-To: $email";
 
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
+if (!mail($to, $subject, $body, $header))
+    http_response_code(500);
 ?>
